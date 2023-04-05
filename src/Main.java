3227e1b48ef769539a5575184420f019ef06aedc
Natalia <natysun07@rambler.ru>
2023-04-05 21:45:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int purchase = 15360;
+        int costBonus = 20;
+        int bonus = purchase / costBonus;
+     System.out.println("Итоговый бонус: " + bonus);
+    }
+
+}
